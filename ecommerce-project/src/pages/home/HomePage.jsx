e6fa11e680bd43dfad9f41ec318e70b6e82df43b
IlkaMatsimnbe <ilkamatsimbe6@gmail.com>
2026-06-04 08:44:21 +0200

    Initial commit - ecommerce fullstack project

---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { Header } from '../../components/Header';
+import './HomePage.css';
+import { useEffect, useState} from 'react';
+import { ProductsGrid } from './ProductsGrid';  
+
+
+export function HomePage({cart, loadCart}) {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    const getHomeData = async () => {
+      const response =  await axios.get('/api/products')
+      setProducts(response.data);
+    };
+
+    
+    getHomeData();
+  }, []);
+
+
+  return (
+    <>
+
+      <Header cart={cart} />
+
+      <title>Ecommerce Project</title>
+
+
+      <div className="home-page">
+        <ProductsGrid products={products} loadCart={loadCart} />
+      </div>
+    </>
+  );
+};
