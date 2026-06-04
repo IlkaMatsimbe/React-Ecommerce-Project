e2f9908f51c6cd47d1f22a7c3aaa9684f702b508
IlkaMatsimnbe <ilkamatsimbe6@gmail.com>
2026-06-04 10:00:44 +0200

    Create new React TypeScript prject

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+  babel: {
+    plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+  },
+})],
   server: {
     proxy: {
       '/api': {
@@ -16,6 +20,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: ''
+    outDir: 'dist',
   }
 })
